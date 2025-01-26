21baa1cb3033030cf2a30cd42c85b16bb3915d17
Mike Flood <flood.mike@gmail.com>
2025-01-25 20:54:15 -0600

    create the paddle

--- src/plug.c
+++ src/plug.c
@@ -3,9 +3,31 @@
 
 #define COLOR_BACKGROUND GetColor(0x151515FF)
 
+#define PADDLE_HEIGHT 20
+#define PADDLE_WIDTH 75
+
+#define BRICK_HEIGHT 20
+#define BRICK_WIDTH 50
+
+static void render(void) {
+	int width = GetScreenWidth();
+	int height = GetScreenHeight();
+
+	Rectangle paddle = {
+		.x = width/4 - 25,
+		.y = height - 50,
+		.width = PADDLE_WIDTH,
+		.height = PADDLE_HEIGHT,
+	};
+
+	DrawRectangleRec(paddle, WHITE);
+}
+
 void plug_update(void) {
 	BeginDrawing();
 	ClearBackground(COLOR_BACKGROUND);
+
+	render();
 
 	EndDrawing();
 }
